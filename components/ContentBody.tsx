cc9ebef998e09cc898c8df85030fd6789d4bd3ab
Michele Zucca <michele.zucca81@gmail.com>
2022-10-11 21:28:19 +0300

    created content body component to display rich text

---
+++ components/ContentBody.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+
+const StyledContentBody = styled.div`
+    p {
+        line-height: 1.6;
+    }
+`;
+
+export const ContentBody = ({ bodyText }) => (
+  <StyledContentBody>{documentToReactComponents(bodyText)}</StyledContentBody>
+);
