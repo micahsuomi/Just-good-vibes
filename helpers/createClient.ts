fabdd6490b4a9b2c7ba164741278bb7862b64ef6
Michele Zucca <michele.zucca81@gmail.com>
2022-10-14 10:56:27 +0300

    testing deployment

--- helpers/createClient.ts
+++ helpers/createClient.ts
@@ -1,6 +1,8 @@
 import { createClient } from "contentful";
 
-export const client = createClient({
-	space: process.env.CONTENTFUL_SPACE_ID,
-	accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-});
+export const client = createClient(
+	process.env.NODE_ENV === "development" && {
+		space: process.env.CONTENTFUL_SPACE_ID,
+		accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+	}
+);
