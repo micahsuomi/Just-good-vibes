43ac240b17167e586cef98d7be98202febc149c7
Michele Zucca <michele.zucca81@gmail.com>
2022-10-16 23:32:47 +0300

    added axios and formspree package, added basic structure for contact form

--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,5 @@
 export * from "./BlogItem";
+export * from "./ContactForm";
 export * from "./ContentBody";
 export * from "./EventItem";
 export * from "./Layout";
---
+++ pages/contact/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { ContactForm } from "../../components";
+export default function ContactPage() {
+  return (
+    <div>
+      <h3>Contact us</h3>
+      <ContactForm />
+    </div>
+  );
+}
