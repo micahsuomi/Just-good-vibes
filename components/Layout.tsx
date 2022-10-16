3e7107a6a28a4728b853f019c6b6866adc7d994d
Michele Zucca <michele.zucca81@gmail.com>
2022-10-16 23:10:37 +0300

    added animations to header for toggle menu and scroll hide and show, added types for components, imported to Layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,57 +1,26 @@
-import React from "react";
-import Link from "next/link";
+import React, { useState } from "react";
 import { LayoutProps } from "../types";
 import styled from "styled-components";
 
-const StyledNavLink = styled.a`
-  font-size: 16px;
-  text-decoration: none;
-  color: rgb(74, 68, 68);
-  display: inline-block;
-  margin: 1rem;
-  font-family: "Courier New";
+import { Header } from "./Header";
+import { OverlayBackground } from "./OverlayBackground";
+
+const StyledLayoutWrapper = styled.div`
+  position: relative;
 `;
 
-const StyledLogo = styled.h4`
-  margin: 1rem 0;
-`;
 export const Layout = ({ children }: LayoutProps) => {
+  const [overlayShowing, setOverlayShowing] = useState(false);
+  const toggleOverlay = (open) => setOverlayShowing(open);
+
   return (
-    <div className="layout">
-      <header>
-        <div>
-          <StyledLogo>Good Vibes</StyledLogo>
-        </div>
-        <div>
-          <Link href="/" passHref>
-            <StyledNavLink>Home</StyledNavLink>
-          </Link>
-          <Link href="/about" passHref>
-            <StyledNavLink>About</StyledNavLink>
-          </Link>
-          <Link href="/reviews" passHref>
-            <StyledNavLink>Reviews</StyledNavLink>
-          </Link>
-          <Link href="/events" passHref>
-            <StyledNavLink>Events</StyledNavLink>
-          </Link>
-          <Link href="/gallery" passHref>
-            <StyledNavLink>Gallery</StyledNavLink>
-          </Link>
-          <Link href="/blog" passHref>
-            <StyledNavLink>Blog</StyledNavLink>
-          </Link>
-          <Link href="/contact" passHref>
-            <StyledNavLink>Contact</StyledNavLink>
-          </Link>
-        </div>
-      </header>
-
+    <StyledLayoutWrapper className="layout">
+      <OverlayBackground open={overlayShowing} />
+      <Header showOverlay={toggleOverlay} />
       <>{children}</>
-
       <footer>
         <p>Copyright 2022 Next Js + Contentful Sample Site</p>
       </footer>
-    </div>
+    </StyledLayoutWrapper>
   );
 };
